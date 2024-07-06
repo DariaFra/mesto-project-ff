2d28fd6502b6e4734d003e214720a081f87fde8c
Daria <frank.darya@yandex.ru>
2024-07-06 13:22:55 +0200

    first_one

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template');
 // @todo: DOM узлы
+const container = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard (cardTemplate, container, element) {
+        const cardElement = cardTemplate.content.querySelector('.places__item').cloneNode(true); 
+        cardElement.querySelector('.card__image').setAttribute('src', element.link);
+        cardElement.querySelector('.card__image').setAttribute('alt', element.name);
+        cardElement.querySelector('.card__title').textContent = element.name;
+        
+        const resetButton = cardElement.querySelector('.card__delete-button');
 
+        resetButton.addEventListener('click', function(){
+            delCard(cardElement);
+        })
+        container.append(cardElement);
+}
 // @todo: Функция удаления карточки
 
+function delCard (cardElement) {
+    cardElement.remove();
+}
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(element => {
+    addCard(cardTemplate, container, element);
+});
