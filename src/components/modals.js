bd4fc3bb168c27c4732fa32a6cd9ffe7e0705119
Daria <frank.darya@yandex.ru>
2024-07-31 09:46:30 +0200

    final edits 6PW

--- src/components/modals.js
+++ src/components/modals.js
@@ -2,20 +2,13 @@
 export const openModal = (popup) => {      
     popup.classList.add('popup_is-opened', 'popup_is-animated');
     document.addEventListener('keydown', closeEsc);
-    popup.addEventListener('click', closePopup);
+    popup.addEventListener('click', closePopupByClick);
 }
 
 export const closeModal = (popup) => {
     popup.classList.remove('popup_is-opened');
     document.removeEventListener('keydown', closeEsc);
-    popup.removeEventListener('click', closePopup);
-}
-
-export const initClosePopups = () => {
-    const popups = document.querySelectorAll('.popup');
-    popups.forEach(popup => {
-        popup.addEventListener('click', closePopup)
-    });
+    popup.removeEventListener('click', closePopupByClick);
 }
 
 function closeEsc(evt) {
@@ -25,7 +18,7 @@ function closeEsc(evt) {
     }
 }
 
-const closePopup = evt => {
+const closePopupByClick = evt => {
     if (evt.target === evt.currentTarget || evt.target.classList.contains('popup__close')) { 
          closeModal(evt.currentTarget); 
     } 
